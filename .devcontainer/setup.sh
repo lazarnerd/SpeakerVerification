8dc65a3998547beb6035f35eefa603e6c1cb559c
Daniel Neururer <neud@zhaw.ch>
2023-01-12 16:18:29 +0000

    prepared poetry project

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -7,7 +7,5 @@ sudo chown -R vscode:vscode /home/vscode
 sudo apt update -y
 sudo apt upgrade -y
 
-pip3 install -r requirements.txt
-
 poetry config virtualenvs.create true
 poetry config virtualenvs.in-project true
